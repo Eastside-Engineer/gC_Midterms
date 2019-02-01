a535f58542abc8df59b087575a9d3590da323f5a
derekquinn <quinnderek@yahoo.com>
2019-02-01 15:01:11 -0500

    pos definition

--- src/midterm_GC/Tester.java
+++ src/midterm_GC/Tester.java
@@ -6,6 +6,7 @@ public class Tester {
 		// TODO Auto-generated method stub
 		System.out.println("We are Super Cool Awesome!");
 		System.out.println("POS stands for what again.");
+		System.out.println("POS Stands for point of sale!!!!!!!!~*~");
 	}
 
 }
