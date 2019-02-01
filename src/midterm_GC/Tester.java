3a6ae8b31b197cbffe36ac04ec7f5c8b38bfc7d2
Theo Eisenstein <theo.eisenstein@gmail.com>
2019-02-01 14:55:06 -0500

    FIrst move

--- src/midterm_GC/Tester.java
+++ src/midterm_GC/Tester.java
@@ -5,6 +5,7 @@ public class Tester {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("We are Super Cool Awesome!");
+		System.out.println("POS stands for what again.");
 	}
 
 }
