27478e580e1a0678b83eda1b5debb2fce88d66df
Cynthia <cturner@ltu.edu>
2019-02-01 15:05:11 -0500

    name changes

--- src/gC_Midterms/POSApp.java
+++ src/gC_Midterms/POSApp.java
@@ -1,12 +1,13 @@
-package midterm_GC;
+package gC_Midterms;
 
-public class Tester {
+public class POSApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("We are Super Cool Awesome!");
 		System.out.println("POS stands for what again.");
 		System.out.println("POS Stands for point of sale!!!!!!!!~*~");
+		System.out.println("Name change!");
 	}
 
 }
