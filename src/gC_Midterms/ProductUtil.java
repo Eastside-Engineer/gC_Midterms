f456a73d9bdc5d617850edce7495182592cda78c
derekquinn <quinnderek@yahoo.com>
2019-02-02 14:31:07 -0500

    updates

--- src/gC_Midterms/ProductUtil.java
+++ src/gC_Midterms/ProductUtil.java
@@ -1,10 +1,30 @@
 package gC_Midterms;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class ProductUtil {
 
+	// get the file path so we can read it in the getPrompt method
+	private static Path filePath = Paths.get("inventory.txt");
+
+	// private or protected????
+	private static List<String> getPrompt() throws IOException {
+
+		List<String> books = Files.readAllLines(filePath);
+
+		return books;
+	}
+
+	private static void checkForFile(Path filePath) throws IOException {
+		if (Files.notExists(filePath)) {
+			Files.createFile(filePath);
+		}
+	}
 }
-
-
 //Change Log:
 //13:54 PM 2/2/2019 - Removed ProductUtil "extends Product"
 //This file just contains methods we will call in the main. This
