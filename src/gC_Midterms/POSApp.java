4821ccb4e7a040862ac2799d61e014477e66cd51
derekquinn <quinnderek@yahoo.com>
2019-02-02 14:54:02 -0500

    menu printing

--- src/gC_Midterms/POSApp.java
+++ src/gC_Midterms/POSApp.java
@@ -7,34 +7,31 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class POSApp {
-	
+
 	private static Path filePath = Paths.get("inventory.txt");
 
-	// possibly need to remove throws IO because not proper practice 
-	
+// possibly need to remove throws IO because not proper practice
+
 	public static void main(String[] args) throws IOException {
-		
-	
-		// methods that prints inventory 
-		
+
+// methods that prints inventory
+
 		getPrompt();
-		
-		
-		
+
 	}
-	
-	// might move this to product util class later 
-	
+
+// might move this to product util class later
+
 	protected static void getPrompt() throws IOException {
-		
-	//	System.out.println("test");
+
+// System.out.println("test");
 
 		List<String> books = Files.readAllLines(filePath);
-		
+// cycle through products and print them on lines
 		for (String thisBook : books) {
 			System.out.println(thisBook);
 
-	
-	}
+		}
 
-}}
+	}
+}
