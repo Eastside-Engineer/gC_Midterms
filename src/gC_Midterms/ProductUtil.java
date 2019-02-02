ceddd6ca339546e17c9d1d2d3383c07c1023d1f1
Theo Eisenstein <theo.eisenstein@gmail.com>
2019-02-02 14:03:52 -0500

    Comments

--- src/gC_Midterms/ProductUtil.java
+++ src/gC_Midterms/ProductUtil.java
@@ -1,5 +1,11 @@
 package gC_Midterms;
 
-public class ProductUtil extends Product {
+public class ProductUtil {
 
 }
+
+
+//Change Log:
+//13:54 PM 2/2/2019 - Removed ProductUtil "extends Product"
+//This file just contains methods we will call in the main. This
+//is not an extension of the product class.
