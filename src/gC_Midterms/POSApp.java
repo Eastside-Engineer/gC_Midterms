5331552b97c5376e0bdb391343f1bd48060bdf22
derekquinn <quinnderek@yahoo.com>
2019-02-04 16:44:09 -0500

    more conditional logic

--- src/gC_Midterms/POSApp.java
+++ src/gC_Midterms/POSApp.java
@@ -20,6 +20,7 @@ public class POSApp {
 		double subTotal = 0;
 		double grandTotal = 0;
 		final double salesTax = 1.06;
+		boolean run = true;
 		List<String> toyCart = new ArrayList<String>();
 		List<Double> toyCartPrice = new ArrayList<Double>();
 		List<Double> toyCartNum = new ArrayList<Double>();
@@ -29,43 +30,64 @@ public class POSApp {
 // methods that prints inventory
 
 		List<Toy> toyList = readFile();
-
-		getPrompt();
+		while (run == true) {
+			getPrompt();
 // begin user input for adding items to SHOPPING CART
-		Scanner scnr = new Scanner(System.in);
-		int userChoice = ProductUtil.getProductNum(scnr, "Enter a product number: ", 1, toyList.size());
-		System.out.println("Do you want to add " + toyList.get(userChoice - 1).getCategory() + ", "
-				+ toyList.get(userChoice - 1).getName() + " to your cart?");
+			Scanner scnr = new Scanner(System.in);
+			int userChoice = ProductUtil.getProductNum(scnr, "Enter a product number: ", 1, toyList.size());
+			System.out.println("Do you want to add " + toyList.get(userChoice - 1).getCategory() + ", "
+					+ toyList.get(userChoice - 1).getName() + " to your cart?");
 //		System.out.println("(y/n); ");
-		// conditional logic to validate user input
-		String userCont = Validator.getStringMatchingRegex(scnr, "(y/n): ", "[yYnN]");
-		if (userCont.equalsIgnoreCase("y")) {
-			double userQuantity = Validator.getDouble(scnr,
-					"How many of " + toyList.get(userChoice - 1).getName() + " would you like to add?:", 0, 500);
-			System.out.println((int) userQuantity + " of " + toyList.get(userChoice - 1).getName()
-					+ " have been added to the cart.");
-			// Adding userQuantity times the price to the subTotal
-			subTotal += userQuantity * toyList.get(userChoice - 1).getPrice();
-			System.out.println("Your Subtotal is:");
-			System.out.printf("$%-9.2f", subTotal);
-			
-			//We are storing the user input inside of para lol arrays. Like a boss.
-			toyCart.add(toyList.get(userChoice-1).getName());
-			toyCartPrice.add(toyList.get(userChoice-1).getPrice());
-			toyCartNum.add(userQuantity);
+			// conditional logic to validate user input
+			String userCont = Validator.getStringMatchingRegex(scnr, "(y/n): ", "[yYnN]");
+
+			if (userCont.equalsIgnoreCase("y")) {
+
+				double userQuantity = Validator.getDouble(scnr,
+						"How many of " + toyList.get(userChoice - 1).getName() + " would you like to add?:", 0, 500);
+				System.out.println((int) userQuantity + " of " + toyList.get(userChoice - 1).getName()
+						+ " have been added to the cart.");
+				// Adding userQuantity times the price to the subTotal
+				subTotal += userQuantity * toyList.get(userChoice - 1).getPrice();
+
+				// We are storing the user input inside of para lol arrays. Like a boss.
+				toyCart.add(toyList.get(userChoice - 1).getName());
+				toyCartPrice.add(toyList.get(userChoice - 1).getPrice());
+				toyCartNum.add(userQuantity);
+				
+				// give user an option to check out after item is added 
+				System.out.println("Would you like to continue shopping?");
+				userCont = Validator.getStringMatchingRegex(scnr, "(y/n): ", "[yYnN]");
+				
+				if (userCont.equalsIgnoreCase("n")){
+					break;
+				}
 //			 grandTotal += subTotal*salesTax;
 
-		} else if (userCont.equalsIgnoreCase("n")) {
-			System.out.println(Validator.getStringMatchingRegex(scnr, "Would you like to look for another toy? (y/n): ",
-					"[yYnN]"));
+			} else if (userCont.equalsIgnoreCase("n")) {
+				System.out.println(Validator.getStringMatchingRegex(scnr,
+						"Would you like to look for another toy? (y/n): ", "[yYnN]"));
 
-		} else {
+				if (userCont.equalsIgnoreCase("n")) {
+					break;
+				}
+
+			} else {
+
+			//	run = false;
 //			System.out.println(Validator.getStringMatchingRegex(scnr, "Display menu, Press 'm'", "[mM]"));
 //			System.out.println();
 
+			}
+			
+			
 		}
 		// int userInput = scnr.nextInt();
-		System.out.println(toyCartNum.toString() + ", "+toyCart.toString()+ ", for a total of $"+toyCartPrice.toString());
+		System.out.println("Thanks for shopping.");
+		System.out.println("\n");
+		System.out.print(toyCartNum.toString() + ", " + toyCart.toString() + ", for a total of ");
+		System.out.printf("$%-9.2f", subTotal);
+
 	} // END MAIN METHOD
 
 // might move this to product util class later
@@ -78,19 +100,19 @@ public class POSApp {
 		List<Toy> toyList = readFile();
 //		for (String thisToy : toyInventory) {
 //			System.out.println(thisToy);
-			for (int i = 0; i < toyList.size(); i++) {
-				// Trying to print the index of the menu.
-				// System.out.printf("%-9s", "\n" + toyList.);
+		for (int i = 0; i < toyList.size(); i++) {
+			// Trying to print the index of the menu.
+			// System.out.printf("%-9s", "\n" + toyList.);
 
-				System.out.print(i + 1 + ". "); // using the incrementor +1 to denote index AKA product nmber for user input
-				System.out.println(toyList.get(i).getName());
-				System.out.printf("%-10s", "$" + toyList.get(i).getPrice());
-				System.out.printf("%-10s \t", toyList.get(i).getCategory());
-				System.out.printf("%-9s", "\n" + toyList.get(i).getDescription());
-				System.out.println("\n");
+			System.out.print(i + 1 + ". "); // using the incrementor +1 to denote index AKA product nmber for user input
+			System.out.println(toyList.get(i).getName());
+			System.out.printf("%-10s", "$" + toyList.get(i).getPrice());
+			System.out.printf("%-10s \t", toyList.get(i).getCategory());
+			System.out.printf("%-9s", "\n" + toyList.get(i).getDescription());
+			System.out.println("\n");
 
-			}
 		}
+	}
 //	}
 
 // creating a list of product objects called "products" and setting it equal to an array list separated b ycom
