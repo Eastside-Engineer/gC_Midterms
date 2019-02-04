a9155d73c1e20b556419b69b10915a3748adfc11
derekquinn <quinnderek@yahoo.com>
2019-02-04 11:38:17 -0500

    dollar  in the right place

--- src/gC_Midterms/POSApp.java
+++ src/gC_Midterms/POSApp.java
@@ -30,9 +30,9 @@ public class POSApp {
 
 		
 			 System.out.println(toyList.get(i).getName());
-			 System.out.printf("%-10s \t", toyList.get(i).getPrice());
+			 System.out.printf("%-10s" ,"$"+ toyList.get(i).getPrice());
 			System.out.printf("%-10s \t", toyList.get(i).getCategory());
-			 System.out.printf("%-9s", "$" + toyList.get(i).getDescription());
+			 System.out.printf("%-9s", "\n" + toyList.get(i).getDescription());
 			 System.out.println("\n");
 
 		}
